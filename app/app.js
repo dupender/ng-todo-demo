24bc9ea21419263becf5702a0d09fd48521fb9e1
Dhruti Upender <dhruti.upender@gmail.com>
2016-05-21 10:37:36 -0500

    added additional imputs lines for New Items

--- app/app.js
+++ app/app.js
@@ -8,6 +8,40 @@ app.controller("NavCtrl", function($scope){
 app.controller("TodoCtrl", function($scope){
     $scope.welcome = "hello";
     $scope.showListView = true;
+    $scope.newTask = {};
+
+    $scope.items = [
+      {
+        id: 0,
+        task:"mow the lawn",
+        isCompleted:true,
+        dueDate:"12/5/17",
+        assignedTo: "greg",
+        location: "zoe's house",
+        urgency: "low",
+        dependancies:["sunshine", "clippers", "hat", "water", "headphones"]
+      },
+      {
+        id: 1,
+        task:"grade quizzes",
+        isCompleted:false,
+        dueDate:"12/5/17",
+        assignedTo: "jow",
+        location: "NSS",
+        urgency: "high",
+        dependancies:["wifi", "tissues", "vodka"]
+      },
+      {
+        id: 2,
+        task:"take a nap",
+        isCompleted:false,
+        dueDate:"12/5/17",
+        assignedTo: "zoe",
+        location: "zoe's house",
+        urgency: "low",
+        dependancies:["hammock", "vodka"]
+      }
+    ]
 
     $scope.newItem = function(){
       console.log("you clicked new Item" );
