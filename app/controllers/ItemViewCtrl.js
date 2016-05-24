36e55e9113233cfb7f136ae9b2b0cfee2469750f
Dhruti Upender <dhruti.upender@gmail.com>
2016-05-23 20:57:25 -0500

    added styling and details

--- app/controllers/ItemViewCtrl.js
+++ app/controllers/ItemViewCtrl.js
@@ -1,4 +1,17 @@
-app.controller("ItemViewCtrl", function($scope){
-    $scope.welcome = "hello";
-    
+app.controller("ItemViewCtrl", function($scope, $http, $routeParams){
+    $scope.items = [];
+    $scope.selectedItem = {}; 
+    console.log($routeParams.itemId);
+
+    $http.get("https://du-angular-todo.firebaseio.com/items.json")
+    .success(function(itemObject){
+      Object.keys(itemObject).forEach(function(key){
+        itemObject[key].id=key;
+        $scope.items.push(itemObject[key]);
+      
+      $scope.selectedItem = $scope.items.filter(function(item){
+        return item.id ===$routeParams.itemId;
+      })[0];
+    })
+    })   
 })
